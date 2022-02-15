ba443d6de8dada5469c5c258e50c041f7b88ef18
RDGS01110 <leorodrigues1234rdgs@gmail.com>
2022-02-15 18:24:37 -0300

    Updated client

--- WhatsApp2/Client/client.py
+++ WhatsApp2/Client/client.py
@@ -5,9 +5,9 @@ import sys
 
 HEADER_LENGTH = 10
 
-now = datetime.now()
+localhost_IP = input("Informe o endereco local do servidor: ")
 
-localhost_IP = input("Informe o IP localhost do servidor: ")
+now = datetime.now()
 
 IP = localhost_IP
 PORT = 5000
@@ -23,16 +23,8 @@ username_header = f"{len(username):<{HEADER_LENGTH}}".encode("utf-8")
 client_socket.send(username_header + username)
 
 while True:
-
-	choose = int(input("Escolha\n1 Ler\n2 Editar"))
-	while choose < 1 or choose > 2:
-		print("Voce deve escolher entre ler e editar somente")
-		choose = int(input("Escolha\n1 Ler\n2 Editar"))
-
-	if choose == 1:
-		message = ""  # reader
-	elif choose == 2:
-		message = input(f"{my_username} > ") #client
+	#message = input(f"{my_username} > ")  # client
+	message = "" #reader
 
 	if message:
 		message = message.encode("utf-8")
@@ -44,7 +36,7 @@ while True:
 			username_header = client_socket.recv(HEADER_LENGTH)
 
 			if not len(username_header):
-				print("Conexao encerrada pelo servidor!")
+				print(now, ": Conexao encerrada pelo servidor!")
 				sys.exit()
 			username_length = int(username_header.decode("utf-8").strip())
 
@@ -61,10 +53,10 @@ while True:
 
 	except IOError as e:
 		if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-			print(now,": ", 'Erro de leitura', str(e))
+			print(now, ": ",'Erro de leitura', str(e))
 			sys.exit()
 		continue
 
 	except Exception as e:
-		print(now,": ", 'Erro geral', str(e))
+		print(now, ":", 'Erro geral', str(e))
 		sys.exit()
